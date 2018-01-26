89519ecea34bf85a55d8f6b333a6bb9f9fbd4caa
Ismayil_Niftaliyev <ismayilturksoy@gmail.com>
2018-01-26 16:43:53 +0300

    implemented some noncompleted parts in logger and appender part

--- log_record.go
+++ log_record.go
@@ -1,11 +1,24 @@
 package golog
 
 import (
+	"bytes"
 	"time"
 )
 
 type LogRecord struct {
-	Time time.Time
-	Level LogLevel
+	Time    time.Time
+	Level   LogLevel
 	Message string
+}
+
+func (lr *LogRecord) ToBuffer() *bytes.Buffer {
+	buffer := &bytes.Buffer{}
+
+	buffer.WriteString(lr.Time.Format(RFC822))
+	buffer.WriteString(" : ")
+	buffer.WriteString(lr.Level.String())
+	buffer.WriteString(" : ")
+	buffer.WriteString(lr.Message)
+
+	return buffer
 }
--- logger.go
+++ logger.go
@@ -1,6 +1,8 @@
 package golog
 
 import (
+	"fmt"
+	"time"
 	"log"
 	"sync"
 )
@@ -16,6 +18,7 @@ type Logger interface {
 
 type defaultLogger struct{
 	lock sync.Mutex
+	logLevel LogLevel
 	appender LogAppender
 	logger *log.Logger
 }
@@ -45,7 +48,24 @@ func (l *defaultLogger) Fatal(format string, args ...interface{})  {
 }
 
 func (l *defaultLogger) log(level LogLevel, format string, args ...interface{})  {
-	
+	if level < l.logLevel {
+		return
+	}
+
+	record := &LogRecord {
+		Level: l.logLevel,
+		Time: time.Now(),
+		Message: fmt.Sprintf(format, args...),
+	}
+
+	go func() {
+		switch level {
+		case TRACE, DEBUG, INFO, WARNING, ERROR:
+			l.logger.Print(record.ToBuffer())
+		case FATAL: 
+		    l.logger.Fatal(record.ToBuffer())
+		}
+	}()
 }
 
 
